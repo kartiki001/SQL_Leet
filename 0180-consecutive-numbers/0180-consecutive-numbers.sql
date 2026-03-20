c602b96ec035ad9ae0e89252997a14b62ccdc1ce
Kartiki <67822105+kartiki001@users.noreply.github.com>
2026-03-20 15:29:09 +0530

    Time: 569 ms (33.45%), Space: 0B (100%) - LeetHub

---
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select distinct t1.num as ConsecutiveNums 
+from Logs t1
+inner join Logs t2
+on t1.id = t2.id+1
+inner join Logs t3 
+on t1.id = t3.id+2
+where t1.num = t2.num and t2.num = t3.num;
